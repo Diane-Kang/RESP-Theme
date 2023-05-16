6a60781bd6bde9114a4f0d45101da048786eb87b
Diane Kang <diane.kang@page-effect.com>
2023-05-16 14:10:34 +0200

    header HTML ready

--- header.php
+++ header.php
@@ -27,17 +27,12 @@
 <body <?php body_class(); ?>>
 
   <?php
-  wp_body_open(); //do_action( 'wp_body_open' ); ex add_action( 'wp_body_open', 'google_tags_manager_body_open_scripts' );
+  // wp_body_open(); //do_action( 'wp_body_open' ); ex add_action( 'wp_body_open', 'google_tags_manager_body_open_scripts' );
   ?>
 
   <!-- Header Before main  -->
-  <?php //get_template_part( 'template-parts/header/site-header' ); 
+  <?php get_template_part( 'template-parts/header/header' ); 
   ?>
-  <header class="header">
-    <div class="header__container container container--wide">
-      header
-    </div>
-  </header>
 
-  <!-- main  -->
-  <main class="main">
+<!-- main  -->
+<main class="main">
---
+++ template-parts/header/header.php
@@ -0,0 +1,48 @@
+<header class="header--outer center">
+  <div class="header flex">
+    <div class="logo flex">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="real">
+    </div>
+    <nav class="nav">
+      <ul class="nav-first">
+        <li><a href="#"><span>Produkte</span> <span class="icon">a</span></a>
+          <!-- First Tier Drop Down -->
+          <ul class="nav-second">
+            <li><a href="#">Markt-Monitor</a>
+              <!-- Second Tier Drop Down -->
+              <ul class="nav-third">
+                <li><a href="#">Funktionen</a></li>
+                <li><a href="#">Referenzen</a></li>
+                <li><a href="#">Preise</a></il>
+                <li><a href="#">Jetzt texten</a></il>
+              </ul>
+            </li>
+            <li><a href="#">Markt-Monitor</a></li>
+            <li><a href="#">Arbeitsplattform</a>
+            <li><a href="#">Immobilien-Rechner</a>
+              <!-- Second Tier Drop Down -->
+              <ul>
+                <li><a href="#">HTML/CSS</a></li>
+                <li><a href="#">jQuery</a></li>
+                <li><a href="#">Other Stuff</a>
+                  <!-- Third Tier Drop Down -->
+                  <ul>
+                    <li><a href="#">Stuff</a></li>
+                    <li><a href="#">Things</a></li>
+                    <li><a href="#">Other Stuff</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li><a href=""><span>Hilfe</span> <span class="icon">a</span></a></li>
+        <li><a href=""><span>Unternehmen</span> <span class="icon">a</span></a></li>
+      </ul>
+    </nav>
+    <div class="header-meta flex">
+      <a href="#">Kontakt</a>
+      <a href="#">Anmelden <span class="icon">a</span></a>
+    </div>
+  </div>
+</header>
