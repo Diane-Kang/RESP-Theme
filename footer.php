b4300909744d5492ed45cbc48cf697bf1f8d4723
Diane Kang <diane.kang@page-effect.com>
2023-05-19 17:30:11 +0200

    Footer started

--- footer.php
+++ footer.php
@@ -9,19 +9,16 @@
 </main><!-- .site-main -->
 
 <footer class="footer">
-  <div class="container container--wide">
-    <div class="footer__newsletter center border">
-      <div class="text">
-        Jetzt über GeoMap informiert bleiben und unseren Newsletter bestellen!
-      </div>
-      <div class="btn btn--empty">
-        Jetzt anmelden
+  <div class="footer__newsletter">
+    newsletter
+  </div>
+  <div class="footer__main">
+    <div class="container">
+      <div class="sitemap">
+        <?php getMenu($menu_name = 'footer', $depth = 2); ?>
       </div>
     </div>
-    <div class="footer__sitemap">
-      <?php getMenu($menu_name = 'footer', $depth = 2); ?>
-      <?php getMenu($menu_name = 'social'); ?>
-    </div>
+    <?php getMenu($menu_name = 'social'); ?>
   </div>
 </footer>
 
--- template-parts/navigation/nav-functions.php
+++ template-parts/navigation/nav-functions.php
@@ -140,28 +140,28 @@ class WPDocs_Walker_Nav_Menu extends Walker_Nav_Menu
     $attributes .= !empty($item->xfn)        ? ' rel="'    . esc_attr($item->xfn) . '"' : '';
     $attributes .= !empty($item->url)        ? ' href="'   . esc_attr($item->url) . '"' : '';
     $attributes .= ' class="menu-link ' . ($depth > 0 ? 'sub-menu-link' : 'main-menu-link') . '"';
-    if ($depth == 0) {
-      $item_output = sprintf(
-        '%1$s<a%2$s>%3$s<span>%4$s</span>%5$s <span class="icon">a</span></a>%6$s',
-        $args->before,
-        $attributes,
-        $args->link_before,
-        apply_filters('the_title', $item->title, $item->ID),
-        $args->link_after,
-        $args->after
-      );
-    } else {
-      // Build HTML output and pass through the proper filter.
-      $item_output = sprintf(
-        '%1$s<a%2$s>%3$s%4$s%5$s</a>%6$s',
-        $args->before,
-        $attributes,
-        $args->link_before,
-        apply_filters('the_title', $item->title, $item->ID),
-        $args->link_after,
-        $args->after
-      );
-    }
+    // if ($depth == 0) {
+    //   $item_output = sprintf(
+    //     '%1$s<a%2$s>%3$s<span>%4$s</span>%5$s <span class="icon">a</span></a>%6$s',
+    //     $args->before,
+    //     $attributes,
+    //     $args->link_before,
+    //     apply_filters('the_title', $item->title, $item->ID),
+    //     $args->link_after,
+    //     $args->after
+    //   );
+    // } else {
+    // Build HTML output and pass through the proper filter.
+    $item_output = sprintf(
+      '%1$s<a%2$s>%3$s%4$s%5$s</a>%6$s',
+      $args->before,
+      $attributes,
+      $args->link_before,
+      apply_filters('the_title', $item->title, $item->ID),
+      $args->link_after,
+      $args->after
+    );
+    // }
     $output .= apply_filters('walker_nav_menu_start_el', $item_output, $item, $depth, $args);
   }
 }
