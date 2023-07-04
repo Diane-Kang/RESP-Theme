748182150240653400551e8996d34d313808dfe3
Diane Kang <diane.kang@page-effect.com>
2023-07-04 10:55:28 +0200

    Module: icons done

--- functions/acfblock.php
+++ functions/acfblock.php
@@ -28,8 +28,8 @@ new ACFblock("imagetext");
 new ACFblock("cards");
 new ACFblock("subnavigation");
 new ACFblock("h-line");
-
 new ACFblock("testimonial");
 new ACFblock("price-table");
 new ACFblock("booking");
 new ACFblock("faq");
+new ACFBlock("logos");
