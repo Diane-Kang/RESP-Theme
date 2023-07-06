801db2e0c630013d8ae13dfae0d7cb422ed892eb
Diane Kang <diane.kang@page-effect.com>
2023-07-06 12:51:30 +0200

    import Module: Kotakt done

--- acf-blocks/contact/contact.php
+++ acf-blocks/contact/contact.php
@@ -17,41 +17,43 @@
 // Common definition of $anchor, $module_classes, $container_classes
 require(get_template_directory() . '/acf-blocks/module-classes.php');
 array_unshift($module_classes, "module", "contact");
-array_unshift($container_classes, "container grid12");
+array_unshift($container_classes, "container");
 
 
 // Content
 $profilbild       = get_field('profilbild');
-$name     		  = get_field('name');
-$rolle     		  = get_field('rolle');
-$gender 	      = get_field('gender');
-$email   	      = get_field('email');
-$telefon   	      = get_field('telefon');
+$name             = get_field('name');
+$rolle            = get_field('rolle');
+$gender           = get_field('gender');
+$email            = get_field('email');
+$telefon          = get_field('telefon');
 $linkedin         = get_field('linkedin');
 
 
 ?>
 <div <?php echo $anchor; ?> class="<?php echo implode(" ", $module_classes);  ?>">
-	<div class="<?php echo implode(" ", $container_classes); ?>">
-      		<div class="profilbild_wrapper">
-			  <img class="profilbild" src="<?php echo esc_url($profilbild['url']); ?>" alt="<?php echo $name; ?>" />
-	  		</div>
-			<div class="details">
-	  			<h4 class="name">
-					<?php echo $name; ?>
-	  			</h4>
-	  			<p class="rolle">
-					<?php echo $rolle; ?>
-				</p>
-	  			<a class="email" href="mailto:<?php echo $email;  ?>">
-					<?php echo $email; ?>
-				</a>
-				<div class="telefon">
-					<?php echo($telefon) ; ?>
-	  			</div>
-				<a class="social" href="<?php echo $linkedin; ?>">
-					<i class="fa-brands fa-linkedin"></i>
-				</a>
-			</div>
-  	</div>
+  <div class="<?php echo implode(" ", $container_classes); ?> center">
+    <div class="container--inner">
+      <div class="profilbild_wrapper">
+        <img class="profilbild" src="<?php echo esc_url($profilbild['url']); ?>" alt="<?php echo $name; ?>" />
+      </div>
+      <div class="details">
+        <h4 class="name">
+          <?php echo $name; ?>
+        </h4>
+        <p class="rolle">
+          <?php echo $rolle; ?>
+        </p>
+        <a class="email" href="mailto:<?php echo $email;  ?>">
+          <?php echo $email; ?>
+        </a>
+        <div class="telefon">
+          <?php echo ($telefon); ?>
+        </div>
+        <a class="social" href="<?php echo $linkedin; ?>">
+          <i class="fa-brands fa-linkedin"></i>
+        </a>
+      </div>
+    </div>
+  </div>
 </div>
